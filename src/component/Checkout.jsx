dc044c78e438a4325a85902a0d9099e3a53ff49c
rkeerthick <rkeerthick1@gmail.com>
2023-10-05 22:19:22 +0530

    5/10/23 first commit:

--- src/component/Checkout.jsx
+++ src/component/Checkout.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { image } from "../assets/image";
 import Subtotal from "./Subtotal";
+import CheckoutProduct from "./CheckoutProduct";
+import { useStateValue } from "../StateProvider";
 
 const Checkout = () => {
+  const [{ basket }, dispatch] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -11,10 +14,23 @@ const Checkout = () => {
         <div className="">
           <h2 className="checkout__title">Your Shopping Basket</h2>
 
-          {/* Checkout Products */}
-          {/* Checkout Products */}
-          {/* Checkout Products */}
-          {/* Checkout Products */}
+          {/* {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+              
+            />
+          ))} */}
+          <CheckoutProduct
+           id="12321341"
+           title="The Lean Startup: How Constant Innovation Creates Radically Successful Businesses Paperback"
+           price={11.96}
+           rating={5}
+           image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+          />
         </div>
       </div>
       <div className="checkout__right">
--- src/component/CheckoutProduct.jsx
+++ src/component/CheckoutProduct.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
-const CheckoutProduct = () => {
+const CheckoutProduct = ({ id, title, image, price, rating }) => {
   return (
-    <div>CheckoutProduct</div>
-  )
-}
+    <div className="checkoutProduct">
+      <img src={image} alt="" className="checkoutProduct__image" />
+      <div className="checkoutProduct__info">
+        <p className="checkoutProduct__title">{title}</p>
+        <p className="checkoutProduct__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="checkoutProduct__rating">
+          {Array(rating)
+            .fill()
+            .map((key) => (
+              <p>🌟</p>
+            ))}
+        </div>
+        <button>Remove item</button>
+      </div>
+    </div>
+  );
+};
 
-export default CheckoutProduct
+export default CheckoutProduct;
